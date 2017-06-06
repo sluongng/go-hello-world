31246fd2e69c1111786c8e26d348e36388d5e78c
Son Luong Ngoc <sluongng@gmail.com>
2017-06-06 07:42:17 +0700

    implemented json struct encoding with time support

--- gorilla-mux/coursesHandler.go
+++ gorilla-mux/coursesHandler.go
@@ -1,12 +1,25 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/satori/uuid"
 )
+
+// Course struct: Exported fields must capitalize first letter
+type Course struct {
+	ID        uuid.UUID	`json:"Id"`
+	Name      string	`json:"Name"`
+	StartTime time.Time	`json:"StartTime"`
+	EndTime   time.Time	`json:"ExpireTime"`
+}
+
+// Courses struct
+type Courses map[string]*Course
 
 // CoursesList is here
 func CoursesList(w http.ResponseWriter, r *http.Request) {
@@ -16,10 +29,21 @@ func CoursesList(w http.ResponseWriter, r *http.Request) {
 
 // CreateCourse is here
 func CreateCourse(w http.ResponseWriter, r *http.Request) {
-	courseID := uuid.NewV4()
+	newCourse := Course{
+		uuid.NewV4(),
+		"",
+		time.Now().UTC(),
+		time.Now().Add(24 * time.Hour).UTC(),
+	}
 
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprintf(w, "Creating a course with UUID: %v\n", courseID)
+	w.Header().Set("Content-Type", "applicaton/json")
+
+	if err := json.NewEncoder(w).Encode(newCourse); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	// fmt.Fprintf(w, "Creating a course with UUID: %v\n", courseID)
 }
 
 // CourseDetail is here
